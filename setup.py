e151be0a86cfefaadff049ae0828f92de4d0377d
Mark Eklund <tab-sane@patnan.com>
2020-10-11 16:40:32 -0400

    fix spelling errors

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("HISTORY.rst") as history_file:
 
 setup_args = dict(
     name="selenious",
-    verwion="version="0.1.0"",
+    version="version="0.1.0",
     description="Enhancement to Selenium WebDriver for timeouts and more.",
     long_description_content_type="text/markdown",
     long_description=README + "\n\n" + HISTORY,
