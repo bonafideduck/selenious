6fcc2fd521eb21f1dbdd703f0c4e9cef3f342bb2
Mark Eklund <bonafideduck@patnan.com>
2021-11-29 21:07:42 -0500

    Bump version: 0.2.0 → 0.2.1

--- selenious/__init__.py
+++ selenious/__init__.py
@@ -2,7 +2,7 @@ from .webdriver_mixin import WebDriverMixin
 
 __author__ = """Mark Eklund"""
 __email__ = "selenious@patnan.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = [
     "WebDriverMixin",
